07cc969927429f2f2e7562224efce9bf75a8b177
ranisharma8206 <ranisharma8206@gmail.com>
2021-02-24 00:02:33 +0530

    Installed socket io client and integrated scanner socket io with flask socket io.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Camera_preview } from './components/Camera_preview';
 import Connect_viewers_screen from './components/Connect_viewers_screen.js'
+import { io } from "socket.io-client";
+
+const socket = io("http://127.0.0.1:5000/");
 
 function App() {
   const [is_connected, set_is_connected] = useState(false);
   const [selected_viewer, set_selected_viewer] = useState('');
+  const [available_viewers, set_available_viewers] = useState([]);
+
+  useEffect(() => {
+    socket.emit("scanner_found");
+    socket.on("send_viewers_list", (connected_viewers_username_list) => {
+      set_available_viewers(connected_viewers_username_list['connected_viewers_username_list']);
+    });
+  }, []);
+
   if (is_connected) {
     return(
       <Camera_preview />
     );
   }
   return (
-    <Connect_viewers_screen selected_viewer = {selected_viewer}  set_selected_viewer = {set_selected_viewer} set_is_connected = {set_is_connected} /> 
+      <Connect_viewers_screen socket={socket} selected_viewer = {selected_viewer}  set_selected_viewer = {set_selected_viewer} set_is_connected = {set_is_connected} available_viewers = {available_viewers} />
   );
 }
 
